7ada21769264d2c04eda7e974f4ff9283405d36a
saikishore222 <saikishore.chsk@gmail.com>
2021-04-20 09:01:28 +0530

    Initial commit

---
+++ src/main/java/com/dreamauthservice/DreamAuthServiceApplication.java
@@ -0,0 +1,13 @@
+package com.dreamauthservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DreamAuthServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DreamAuthServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/dreamauthservice/DreamAuthServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dreamauthservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DreamAuthServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
